5bdafcdbe8fd89bd9d09d4d752025aea527851a2
scarab <scarab@hmws04.2e2v.ru>
2024-02-21 19:23:01 +0300

    last commit

--- dep/deploy-game-engine.sh
+++ dep/deploy-game-engine.sh
@@ -1,8 +1,9 @@
 #/bin/bash
+export DGE_PATH=.
 mvn deploy:deploy-file -Durl=file:./lib \
                        -DrepositoryId=com.javarush \
                        -DgroupId=com.javarush \
                        -DartifactId=desktop-game-engine \
-                       -Dfile=/home/scarab/Downloads/_Install/Develop/desktop-game-engine.jar \
+                       -Dfile=$DGE_PATH/desktop-game-engine.jar \
                        -Dpackaging=jar \
                        -Dversion=1.0
